bf96769bb08190b6ef6777199b19e170d43046b4
MarioTiara <mario.tiarapratama@gmail.com>
2022-11-27 21:34:27 +0700

    [CloudCustomers.UnitTests] Add User Fixture

---
+++ CloudCustomers.UnitTests/Fixtures/UserFixture.cs
@@ -0,0 +1,53 @@
+﻿using CloudCustomers.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudCustomers.UnitTests.Fixtures
+{
+    public class UserFixture
+    {
+        public static List<User> GetTestUsers() =>
+            new()
+            {
+                new()
+                {
+                    Name="Mario",
+                    Email="mario@gmail.com",
+                    Address= new Address()
+                    {
+                        Street="15 Flamboyan",
+                        City="Bnegkulu",
+                        ZipCode="5374"
+                    }
+                   
+                },
+                 new()
+                {
+                    Name="Pratama",
+                    Email="pratama@gmail.com",
+                    Address= new Address()
+                    {
+                        Street="16 Flamboyan",
+                        City="Bnegkulu Tengah",
+                        ZipCode="5375"
+                    },
+
+                },
+                   new()
+                {
+                    Name="Ramayana",
+                    Email="Ramayana@gmail.com",
+                    Address= new Address()
+                    {
+                        Street="17 Flamboyan",
+                        City="Bnegkulu Utara",
+                        ZipCode="5377"
+                    }
+
+                }
+            };
+    }
+}
