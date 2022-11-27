4a138eef2475ca9758001908035b2033ca53c443
MarioTiara <mario.tiarapratama@gmail.com>
2022-11-27 23:15:08 +0700

    [CloudCustomers.UnitTest] Add GetAllUsers_WhenCalled_Returns404

--- CloudCustomers.UnitTests/Systems/Services/UserServicesTests.cs
+++ CloudCustomers.UnitTests/Systems/Services/UserServicesTests.cs
@@ -2,6 +2,7 @@
 using CloudCustomers.API.Services;
 using CloudCustomers.UnitTests.Fixtures;
 using CloudCustomers.UnitTests.Utils;
+using FluentAssertions;
 using Moq;
 using Moq.Protected;
 using System;
@@ -34,6 +35,37 @@ namespace CloudCustomers.UnitTests.Systems.Services
                 ItExpr.Is<HttpRequestMessage>(req => req.Method == HttpMethod.Get),
                 ItExpr.IsAny<CancellationToken>()
                 );
+        }
+
+        [Fact]
+        public async Task GetAllUsers_WhenCalled_ReturnsListOfUsers()
+        {
+            //Arrange
+            var expectedResponse = UserFixture.GetTestUsers();
+            var hanlderMock = MockHttpMessageHandler<User>
+                .SetupBasicGetResourceList(expectedResponse);
+            var httpClient = new HttpClient(hanlderMock.Object);
+            var sut = new UsersService(httpClient);
+
+            //Act
+            var result = await sut.GetAllUsers();
+            //Assert
+            result.Should().BeOfType<List<User>>();
+        }
+
+        [Fact]
+        public async Task GetAllUsers_WhenCalled_Returns404()
+        {
+            //Arrange
+            var hanlderMock = MockHttpMessageHandler<User>
+                .SetupReturn404();
+            var httpClient = new HttpClient(hanlderMock.Object);
+            var sut = new UsersService(httpClient);
+
+            //Act
+            var result = await sut.GetAllUsers();
+            //Assert
+            result.Count.Should().Be(0);
         }
     }
 }
