b5b503535e9a784035fbfc7d99b6fc37d2b6ba61
MarioTiara <mario.tiarapratama@gmail.com>
2022-11-27 22:56:50 +0700

    [CloudCustomers.API] Add HttpClinet Injection

--- CloudCustomers.API/Program.cs
+++ CloudCustomers.API/Program.cs
@@ -9,6 +9,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddTransient<IUsersService, UsersService>();
+builder.Services.AddHttpClient<IUsersService, UsersService>();
 
 var app = builder.Build();
 
